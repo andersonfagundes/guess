5931a86ea0e70b769cc4954638c4750dc0815976
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-11-14 13:21:40 +0000

    feat: add Letter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './app.module.css';
 import { Header } from './components/Header';
+import { Letter } from './components/Letter';
 import { Tip } from './components/Tip';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="One of the most widely used programming languages" />
+        <Letter value="R" />
       </main>
     </div>
   );
