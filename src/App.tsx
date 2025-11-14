63ac2c2fd4bb237aee82d6c424fb8629a89c6b8e
Anderson Fagundes <anderson.fagundes4@gmail.com>
2025-11-14 12:04:30 +0000

    feat: finalize header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import styles from './app.module.css';
 import { Header } from './components/Header';
 
 export default function App() {
+  function handleRestartGame() {
+    alert('Restart game');
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleRestartGame} />
       </main>
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,25 @@
 import logo from '../../assets/logo.png';
+import restart from '../../assets/restart.svg';
 import styles from './styles.module.css';
 
-export function Header() {
+type Props = {
+  current: number;
+  max: number;
+  onRestart: () => void;
+};
+
+export function Header({ current, max, onRestart }: Props) {
   return (
     <div className={styles.container}>
       <img src={logo} alt="logo" />
+      <header>
+        <span>
+          <strong>{current}</strong> out of {max} attempts
+        </span>
+        <button onClick={onRestart}>
+          <img src={restart} alt="Restart icon" />
+        </button>
+      </header>
     </div>
   );
 }
